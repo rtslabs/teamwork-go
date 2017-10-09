078726e1f04a4e7d7c0a1e8e9fffafa9eae77ceb
gabeduke <gabeduke@gmail.com>
2017-10-09 12:18:31 -0400

    add tasks

---
+++ lib/tasks.go
@@ -0,0 +1,67 @@
+package lib
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
+
+type TaskLists struct {
+	ExecutionTime   string     `json:"executionTime"`
+	ProjectBeanList []TaskList `json:"tasklists"`
+}
+
+type TaskList struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type Tasks struct {
+	Id           string `json:"id"`
+	TaskBeanList []Task `json:"todo-items"`
+}
+
+type Task struct {
+	Id          int    `json:"id"`
+	ProjectName string `json:"project-name"`
+}
+
+func GetTasks(tasklistId string) (*Tasks, error) {
+	resp := GetRequest("/tasklists/" + tasklistId + "/tasks.json")
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s, err := unpackTasks([]byte(body))
+
+	return s, err
+}
+
+func GetTaskLists(projectId string) (*TaskLists, error) {
+
+	resp := GetRequest("projects/" + projectId + "/tasklists.json")
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var s = new(TaskLists)
+	err = json.Unmarshal(body, &s)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return s, err
+}
+
+func unpackTasks(body []byte) (*Tasks, error) {
+	var s = new(Tasks)
+	err := json.Unmarshal(body, &s)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return s, err
+}
