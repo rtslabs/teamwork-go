efe18d36e95ee53653f5a2dec4c9f6988a9350a1
gabeduke <gabeduke@gmail.com>
2017-10-10 17:30:48 -0400

    add prompt

--- cmd/prompt.go
+++ cmd/prompt.go
@@ -23,15 +23,32 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/c-bata/go-prompt"
 	"github.com/spf13/cobra"
-	_ "github.com/mattn/go-sqlite3"
-	"github.com/abiosoft/ishell"
-	"teamworkgo/db"
 	"log"
+	"os"
+	"os/exec"
+	"teamworkgo/db"
+	"strings"
 )
 
-// promptCmd represents the prompt command
-var promptCmd = &cobra.Command{
+var projectsSuggestions = []prompt.Suggest{}
+var taskListsSuggestions = []prompt.Suggest{}
+var taskSuggestions = []prompt.Suggest{}
+var commands = []prompt.Suggest{
+	{Text: "ls", Description: "List the current working tree"},
+	{Text: "cd", Description: "change dir"},
+
+	// aliases
+	{Text: "list"},
+
+	// customized
+	{Text: "bash", Description: "Drop to a bash subshell"},
+	{Text: "exit", Description: "Exit the prompt"},
+}
+
+// completerCmd represents the completer command
+var completerCmd = &cobra.Command{
 	Use:   "prompt",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
@@ -40,47 +57,111 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+
 	Run: func(cmd *cobra.Command, args []string) {
-		shell := ishell.New()
-
-		shell = registerLs(shell)
-
-		shell.Run()
+		fmt.Printf("this-prompt\n")
+		fmt.Println("Please use `exit` or `Ctrl-D` to exit this program..")
+		defer fmt.Println("Bye!")
+		t := prompt.Input(">>> ", completer, prompt.OptionMaxSuggestion(20))
+		executor(t)
 	},
 }
 
-func registerLs(shell *ishell.Shell) *ishell.Shell {
-	shell.AddCmd(&ishell.Cmd{
-		Name: "ls",
-		Help: "list projects and items",
-		Func: func(c *ishell.Context) {
-			rows := db.GetProjects()
-			for rows.Next() {
-				var id string
-				var projectName string
-				if err := rows.Scan(&id, &projectName); err != nil {
-					log.Fatal(err)
-				}
-				fmt.Printf("%s - %s\n", id, projectName)
-			}
-			if err := rows.Err(); err != nil {
-				log.Fatal(err)
-			}
-		},
-	})
-	return shell
+func executor(t string) {
+	if t == "bash" {
+		cmd := exec.Command("bash")
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Run()
+	}
+	return
+}
+
+func completer(d prompt.Document) []prompt.Suggest {
+	if d.TextBeforeCursor() == "" {
+		return []prompt.Suggest{}
+	}
+	args := strings.Split(d.TextBeforeCursor(), " ")
+	//w := d.GetWordBeforeCursor()
+
+	// If PIPE is in text before the cursor, returns empty suggestions.
+	for i := range args {
+		if args[i] == "|" {
+			return []prompt.Suggest{}
+		}
+	}
+
+	//s := argumentsCompleter
+	//return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
+	return argumentsCompleter(args)
+}
+
+func argumentsCompleter(args []string) []prompt.Suggest {
+	if len(args) <= 1 {
+		return prompt.FilterHasPrefix(commands, args[0], true)
+	}
+
+	first := args[0]
+	switch first {
+	case "projects", "proj":
+		second := args[1]
+		subcommands := projectsSuggestions
+		return prompt.FilterHasPrefix(subcommands, second, true)
+	case "tasklists":
+		third := args[2]
+		subcommands := taskListsSuggestions
+		return prompt.FilterHasPrefix(subcommands, third, true)
+	case "tasks":
+		fourth := args[3]
+		subcommands := taskSuggestions
+		return prompt.FilterHasPrefix(subcommands, fourth, true)
+	default:
+		return []prompt.Suggest{}
+	}
+
+	return []prompt.Suggest{}
+}
+
+func mapProjects() []prompt.Suggest {
+	rows := db.GetProjects()
+	suggestions := []prompt.Suggest{}
+
+	for rows.Next() {
+		var r prompt.Suggest
+		err := rows.Scan(&r.Description, &r.Text)
+		if err != nil {
+			log.Fatal(err)
+		}
+		suggestions = append(suggestions, r)
+	}
+
+	return suggestions
+}
+
+func initializePrompt() {
+	// Get the commands from Cobra to build the prompt suggestions
+	projects := mapProjects()
+	for _, project := range projects {
+		var promptSuggestion = prompt.Suggest{
+			Text:        project.Text,
+			Description: project.Description,
+		}
+		projectsSuggestions = append(projectsSuggestions, promptSuggestion)
+	}
 }
 
 func init() {
-	RootCmd.AddCommand(promptCmd)
+	RootCmd.AddCommand(completerCmd)
+	initializePrompt()
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// promptCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// completerCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// promptCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// completerCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
