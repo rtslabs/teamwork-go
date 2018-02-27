36f37133787be6141f923cc2e035c1177e273c22
gabeduke <gabeduke@gmail.com>
2018-02-26 22:31:47 -0500

    * add short helps * remove long helps * add p flags for config

--- cmd/config.go
+++ cmd/config.go
@@ -9,16 +9,24 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	initConf		*bool
+
+	addName 		*string
+	addTaskID 		*int
+	addTaskListID 	*int
+	addProjectID 	*int
+	addDate 		*string
+	addMessage 		*string
+	addHours 		*int
+	addMinutes 		*int
+	addBillable 	*bool
+)
+
 // configCmd represents the config command
 var configCmd = &cobra.Command{
 	Use:   "config",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "I dont know what the parent command does yet /shrug",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("config called")
 	},
@@ -27,13 +35,7 @@ to quickly create a Cobra application.`,
 // addCmd represents the add command
 var getCmd = &cobra.Command{
 	Use:   "get",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Returns the current project config",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("get called")
 	},
@@ -42,15 +44,18 @@ to quickly create a Cobra application.`,
 // setCmd represents the add command
 var setCmd = &cobra.Command{
 	Use:   "set",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Sets a config in the current project directory",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("set called")
+	},
+}
+
+// initCmd represents the add command
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Initializes a config in the current working directory",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("init called")
 	},
 }
 
@@ -58,31 +63,28 @@ to quickly create a Cobra application.`,
 var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		flagKeys := []string{
+			"addName",
+			"addTaskID",
+			"addTaskListID",
+			"addProjectID",
+			"addDate",
+			"addMessage",
+			"addHours",
+			"addMinutes",
+			"addBillable",
+		}
+
+		for _, key := range flagKeys {
+			//TODO set items to config file
+			fmt.Println("Key: {0}", key)
+		}
+
 		fmt.Println("add called")
 	},
 }
 
-// initCmd represents the add command
-var initCmd = &cobra.Command{
-	Use:   "init",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("init called")
-	},
-}
 
 func init() {
 	configCmd.AddCommand(getCmd)
@@ -92,12 +94,15 @@ func init() {
 	RootCmd.AddCommand(configCmd)
 
 	// Here you will define your flags and configuration settings.
+	configCmd.PersistentFlags().BoolVarP(initConf, "init", "i", false, "Initialize a config in the cwd (defaults to false)")
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// configCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// configCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	addCmd.PersistentFlags().StringVarP(addName, "name", "n", "", "Add an alias [favorite] name to the current working config")
+	addCmd.PersistentFlags().IntVarP(addTaskID, "taskId", "t", 0,"Add a Task ID for the current working config")
+	addCmd.PersistentFlags().IntVarP(addTaskListID, "taskListId", "l", 0, "Add a Task List ID for the current working config")
+	addCmd.PersistentFlags().IntVarP(addProjectID, "projectId", "p", 0, "Add a Project ID for the current working config")
+	addCmd.PersistentFlags().StringVarP(addDate, "date", "d", "", "Add a date to the current working config (mm/dd/yy)")
+	addCmd.PersistentFlags().StringVarP(addMessage, "msg", "m", "", "Add a message to the current working config")
+	addCmd.PersistentFlags().IntVarP(addHours, "hours", "h", 0, "Add default hours to the current working config")
+	addCmd.PersistentFlags().IntVar(addMinutes, "min", 0, "Add default minutes to the current working config")
+	addCmd.PersistentFlags().BoolVarP(addBillable, "billable", "b", true, "Set current working config as billable (defaults to true)")
 }
