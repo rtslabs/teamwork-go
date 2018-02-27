e86c39f7a28dfeb2d55ea28e250a21d12d82b055
gabeduke <gabeduke@gmail.com>
2018-02-26 22:49:53 -0500

    refactor a bit

--- cmd/config.go
+++ cmd/config.go
@@ -10,23 +10,25 @@ import (
 )
 
 var (
-	initConf		*bool
+	initConf		bool
 
-	addName 		*string
-	addTaskID 		*int
-	addTaskListID 	*int
-	addProjectID 	*int
-	addDate 		*string
-	addMessage 		*string
-	addHours 		*int
-	addMinutes 		*int
-	addBillable 	*bool
+	setName 		string
+	setTaskID 		int
+	setTaskListID 	int
+	setProjectID 	int
+	setDate 		string
+	setMessage 		string
+	setHours 		int
+	setMinutes 		int
+	setBillable 	bool
 )
 
 // configCmd represents the config command
 var configCmd = &cobra.Command{
 	Use:   "config",
-	Short: "I dont know what the parent command does yet /shrug",
+	Short: "/shrug I dont know what the parent command does yet",
+	Long: `This could potentially list the current config or print some kind
+	of stats for the current project.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("config called")
 	},
@@ -41,12 +43,12 @@ var getCmd = &cobra.Command{
 	},
 }
 
-// setCmd represents the add command
-var setCmd = &cobra.Command{
-	Use:   "set",
-	Short: "Sets a config in the current project directory",
+// addCmd represents the add command
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "/shrug I don't know what this command does yet",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("set called")
+		fmt.Println("add called")
 	},
 }
 
@@ -59,21 +61,21 @@ var initCmd = &cobra.Command{
 	},
 }
 
-// addCmd represents the add command
-var addCmd = &cobra.Command{
-	Use:   "add",
-	Short: "A brief description of your command",
+// setCmd represents the add command
+var setCmd = &cobra.Command{
+	Use:   "set",
+	Short: "Set config parameters to the current working project config",
 	Run: func(cmd *cobra.Command, args []string) {
 		flagKeys := []string{
-			"addName",
-			"addTaskID",
-			"addTaskListID",
-			"addProjectID",
-			"addDate",
-			"addMessage",
-			"addHours",
-			"addMinutes",
-			"addBillable",
+			"setName",
+			"setTaskID",
+			"setTaskListID",
+			"setProjectID",
+			"setDate",
+			"setMessage",
+			"setHours",
+			"setMinutes",
+			"setBillable",
 		}
 
 		for _, key := range flagKeys {
@@ -81,7 +83,7 @@ var addCmd = &cobra.Command{
 			fmt.Println("Key: {0}", key)
 		}
 
-		fmt.Println("add called")
+		fmt.Println("set called")
 	},
 }
 
@@ -94,15 +96,15 @@ func init() {
 	RootCmd.AddCommand(configCmd)
 
 	// Here you will define your flags and configuration settings.
-	configCmd.PersistentFlags().BoolVarP(initConf, "init", "i", false, "Initialize a config in the cwd (defaults to false)")
+	configCmd.PersistentFlags().BoolVarP(&initConf, "init", "i", false, "Initialize a config in the cwd (defaults to false)")
 
-	addCmd.PersistentFlags().StringVarP(addName, "name", "n", "", "Add an alias [favorite] name to the current working config")
-	addCmd.PersistentFlags().IntVarP(addTaskID, "taskId", "t", 0,"Add a Task ID for the current working config")
-	addCmd.PersistentFlags().IntVarP(addTaskListID, "taskListId", "l", 0, "Add a Task List ID for the current working config")
-	addCmd.PersistentFlags().IntVarP(addProjectID, "projectId", "p", 0, "Add a Project ID for the current working config")
-	addCmd.PersistentFlags().StringVarP(addDate, "date", "d", "", "Add a date to the current working config (mm/dd/yy)")
-	addCmd.PersistentFlags().StringVarP(addMessage, "msg", "m", "", "Add a message to the current working config")
-	addCmd.PersistentFlags().IntVarP(addHours, "hours", "h", 0, "Add default hours to the current working config")
-	addCmd.PersistentFlags().IntVar(addMinutes, "min", 0, "Add default minutes to the current working config")
-	addCmd.PersistentFlags().BoolVarP(addBillable, "billable", "b", true, "Set current working config as billable (defaults to true)")
+	setCmd.PersistentFlags().StringVarP(&setName, "name", "n", "", "Set an alias [favorite] name to the current working config")
+	setCmd.PersistentFlags().IntVarP(&setTaskID, "taskId", "t", 0,"Set a Task ID for the current working config")
+	setCmd.PersistentFlags().IntVarP(&setTaskListID, "taskListId", "l", 0, "Set a Task List ID for the current working config")
+	setCmd.PersistentFlags().IntVarP(&setProjectID, "projectId", "p", 0, "Set a Project ID for the current working config")
+	setCmd.PersistentFlags().StringVarP(&setDate, "date", "d", "", "Set a date to the current working config (mm/dd/yy)")
+	setCmd.PersistentFlags().StringVarP(&setMessage, "msg", "m", "", "Set a message to the current working config")
+	setCmd.PersistentFlags().IntVar(&setHours, "hours", 0, "Set default hours to the current working config")
+	setCmd.PersistentFlags().IntVar(&setMinutes, "min", 0, "Set default minutes to the current working config")
+	setCmd.PersistentFlags().BoolVarP(&setBillable, "billable", "b", true, "Set current working config as billable (defaults to true)")
 }
